4b08340437eb5dd69e64dbc2930e9578a5248732
Vinicius Ribas <vinicius_batista@gec.inatel.br>
2024-06-25 19:25:34 -0300

    Ajustes do codigo

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { getUsers } from '../controller/userController';
+import { getUsers, createUser } from '../controller/userController';
 
 const router = Router();
 
 router.get('/', getUsers);
-// router.post('/', createUser);
+router.post('/', createUser);
 // router.get('/:id', getUserById);
 // router.put('/:id', updateUser);
 // router.delete('/:id', deleteUser);
